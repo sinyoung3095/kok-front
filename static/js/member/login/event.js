584d2df34872181663d83f9932ee1c31b3da3e3c
anna0121222 <anna012188@gmail.com>
2025-09-12 17:10:34 +0900

    member/login

--- static/js/member/login/event.js
+++ static/js/member/login/event.js
@@ -39,3 +39,41 @@ passwordInput.addEventListener("keyup", (e) => {
         loginButton.classList.add('login-disabled');
     }
 });
+
+
+// 토스트 - 로그인 오류
+const isValidEmail = (value) => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value.trim());
+};
+const toastLogin = document.querySelector("#toast-login");
+function showLoginErrorToast() {
+    toastLogin.classList.add("show");
+
+    // 3초 후에 토스트 메시지 숨기기
+    setTimeout(() => {
+        toastLogin.classList.remove("show");
+    }, 3000);
+}
+
+loginButton.addEventListener("click", (e) => {
+    if (!isValidEmail(emailInput.value)) {
+        showLoginErrorToast();
+    }
+});
+
+// 토스트 - 이메일 입력 오류
+const keywordInput = document.querySelector("#keyword-input");
+const toastKeyword = document.querySelector("#toast-keyword");
+function checkKeywordLength() {
+    if (keywordInput && toastKeyword) {
+        if (keywordInput.value.length > 50) {
+            toastKeyword.classList.add("show");
+
+            setTimeout(() => {
+                toastKeyword.classList.remove("show");
+            }, 2000);
+        } else {
+            toastKeyword.classList.remove("show");
+        }
+    }
+}
